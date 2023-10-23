886d89e9df68331c431bb462270bb70174b506da
Axelev <87axelev@gmail.com>
2023-10-23 21:19:11 +0200

    Added basic components

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Employees in Axelson Company</h1>
+            <h2>Total ammount of employees</h2> 
+            <h2>Can be awarded by premie:</h2>          
+        </div>
+    )
+}
+
+export default AppInfo;
---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,11 @@
+import './search-panel.css';
+
+const SearchPanel = () => {
+    return (
+        <input type="text" 
+               className="form-control seacrh-input"
+               placeholder="Find employee" />
+    )
+}
+
+export default SearchPanel;
