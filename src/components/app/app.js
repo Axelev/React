9b93951a9baa7ef689f0f051d349499363c85720
Axelev <87axelev@gmail.com>
2023-10-30 21:57:23 +0100

    added functionality to search bar

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,7 +16,8 @@ class App extends Component {
                 {name: 'John Wick', salary: 300, highlight: false, increase: false, key: 1},
                 {name: 'Bob Marley', salary: 400, highlight: false, increase: false, key: 2},
                 {name: 'Brandon Smith', salary: 1500, highlight: false, increase: false, key: 3},
-            ]
+            ],
+            term: ''
         }
         this.maxKey = 4;
     };
@@ -63,18 +64,36 @@ class App extends Component {
         }))
     }
 
+    onUpdateSearch = (term) => {
+        this.setState({term});
+    }
+
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items
+        }
+
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+        })
+    }
+
     render() {
+        const {data, term} = this.state;
+        const visibleData = this.searchEmp(data,term);
+
+
         return (
             <div className="app">
                 <AppInfo
-                    numberOfEmp={this.state.data}/>
-    
+                    numberOfEmp={data}/>   
                 <div className='search-panel'>
-                    <SearchPanel/>
+                    <SearchPanel
+                    onUpdateSearch={this.onUpdateSearch}/>
                     <AppFilter/>
                 </div>
                 <EmployeesList 
-                    data={this.state.data}
+                    data={visibleData}
                     onDelete={this.deleteItem}
                     onToggleProp={this.onToggleProp}/>
                 <EmployeesAddForm
